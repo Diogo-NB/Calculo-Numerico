b8b1d48bcd114235eeeee380152655e795b4533e
Diogo-NB <113485393+Diogo-NB@users.noreply.github.com>
2023-09-01 17:11:09 -0300

    FIxed trabalho 7

--- Modulo 2/interpolacao_quadratica.py
+++ Modulo 2/interpolacao_quadratica.py
@@ -18,35 +18,37 @@ x3 = 1
 x4 = nova_estimativa(x1, x2, x3)
 
 erroMin = 1e-8
-erro = np.inf
+erro = 1
 
 if f(x2) > f(x4): xot = x2
 else: xot = x4
+i = 0
 
-while erro >= erroMin:
+while erro > erroMin:
+    i+=1
     xot_anterior = xot
 
     if x4 > x2:
         if f(x4) > f(x2):
-            xot = x4
             x1 = x2
             x2 = x4
         else:
-            xot = x2
             x3 = x4
     else:
         if f(x4) > f(x2):
-            xot = x4
             x3 = x2
             x2 = x4
         else:
-            xot = x2
             x1 = x4
-            
-    erro = abs((xot - xot_anterior) / xot)
     x4 = nova_estimativa(x1, x2, x3)
 
+    if f(x4) > f(x2): xot = x4
+    else: xot = x2
+
+    if i > 1:
+        erro = abs((xot - xot_anterior) / xot)
 
 print("Ponto máximo estimado = ", [xot,f(xot)])
+print('it: ', i)
     
 
