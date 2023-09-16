d40106819813c34d26d469a04b5399a2474e76d9
Diogo-NB <113485393+Diogo-NB@users.noreply.github.com>
2023-09-15 23:54:57 -0300

    fix gauss_jacobi

--- Modulo 2/gauss_jacobi.py
+++ Modulo 2/gauss_jacobi.py
@@ -7,26 +7,72 @@ Módulo 2 - Trabalho 8
 
 """
 
-def isolar(i, v):
-    #res = -v/v[i]
-    res = -v / v[i]
-    res[i] = 0
-    return res
+def testSolution(A, x):
+    x = list(x)
+    x.append(1)
+    print(A.dot(x))
 
-A = np.array([[2,1,-20],[1,4,-45]])
-C = np.zeros(A.shape)
-G = np.zeros(A.shape[0])
+# Retorna um novo vetor ao "isolar" a variável da posição i
+def isolar(i: int, v: object):
+    new_v = v * (-1/v[i])
+    new_v[i] = 0
+    return new_v
 
-qtdL = A.shape[0]
-qtdC = A.shape[1]
+# Printa o vetor x
+def print_x(v: object):
+    for i in range(v.shape[0]):
+        print("x" + str(i + 1) + " = " + str(v[i]))
 
-for i in range(qtdL):
-    G[i] = A[i][A.shape[1] -1]
-    print(G[i])
+A = [
+    [8, -3, 2, 1, 0, -1], 
+    [0, -10, 1, 1, -2, -1/2],
+    [1, 0, 9, -1, 1, -3],
+    [-2, 1, 0, -11, 1, 3/2],
+    [-2, 0, 2, 0, 10, 2]
+    ]
+x = [0, 0, 0, 0, 0]
+
+x_anterior = x
+tolerancia = 0.02
+mr = 1
+
+A = np.array(A, dtype = np.double)
+x = np.array(x, dtype = np.double)
+
+# Shape = (nº de linhas, nº de colunas)
+nl = A.shape[0] # nº de linhas
+nc = A.shape[1] # nº de colunas
+
+# Matriz C: Matriz A sem as constantes
+C = np.zeros((nl, nc - 1), dtype = np.double)
+# Vetor g: constantes da matriz A
+g = np.zeros(nl, dtype = np.double)
+
+# Obter C e g
+for i in range(nl):
+    aux = isolar(i, A[i])
+    for j in range(nc - 1):
+        C[i][j] = aux[j]
+    g[i] = aux[nc-1]
+
+# Iterando
+while mr > tolerancia:
+    x = C.dot(x_anterior) + g
+    m = max(abs(x - x_anterior))
+    mr = m / max(x)
+    x_anterior = x
+
+print(x)
+print_x(x)
+
+testSolution(A, x)
 
 
 
-# Shape = (l, c)
+
+
+
+
 
 
     
