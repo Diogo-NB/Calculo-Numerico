0592fc78e69abda8c65c200cc7d72e4111f6a589
Diogo-NB <113485393+Diogo-NB@users.noreply.github.com>
2023-09-28 00:38:16 -0300

    Fixed gauss_jacobi/seidel

--- Modulo 2/gauss_jacobi.py
+++ Modulo 2/gauss_jacobi.py
@@ -9,8 +9,8 @@ Módulo 2 - Trabalho 8
 
 # Retorna um novo vetor ao "isolar" a variável da posição i
 def isolar(i: int, v: object):
-    new_v = v * (-1/v[i])
-    new_v[i] = 0
+    new_v = np.array(v * (-1/v[i]), dtype= "float64")
+    new_v[i] = 0.0
     return new_v
 
 # Printa o vetor x
@@ -23,57 +23,64 @@ A = [
     [0, -10, 1, 1, -2, -1/2],
     [1, 0, 9, -1, 1, -3],
     [-2, 1, 0, -11, 1, 3/2],
-    [-2, 0, 2, 0, 10, 2]
+    [-2, 0, 2, 0, 10, -2]
     ]
 x = [0, 0, 0, 0, 0]
 
+'''
+A = [
+    [2, 1, -20],
+    [1, 4, -45]
+    ]
+x = [0, 0]
+'''
+ 
 tolerancia = 0.02
 mr = 1
 
-A = np.array(A, dtype = np.double)
-x = np.array(x, dtype = np.double)
+A = np.array(A, dtype="float64")
+x = np.array(x, dtype="float64")
 
-# Matriz C: Matriz A sem as constantes
-# A.shape = (nº de linhas de A, nº de colunas de A)
-nl = A.shape[0] # nº de linhas de A e de C
-nc = A.shape[1] - 1 # nº de colunas de C
-C = np.zeros((nl, nc), dtype = np.double)
-# Vetor g: constantes da matriz A
-g = np.zeros(nl, dtype = np.double)
+# Número de linhas 
+nl = A.shape[0]
+# Número de colunas para a matriz C
+nc = nl
 
-# Obter C e g
+C = np.zeros((nl, nc))
+g = np.zeros(nl)
+
+# Encontrando a matriz C e o vetor g
 for i in range(nl):
     aux = isolar(i, A[i])
     for j in range(nc):
         C[i][j] = aux[j]
     g[i] = aux[nc]
 
+# Contador
+k = 0
+
 # Iterando
 while mr > tolerancia:
-    x_anterior = np.array(x)
-    x = C.dot(x_anterior) + g
-    m = max(abs(x - x_anterior))
-    mr = m / max(x)
+    x_anterior = np.copy(x) # Vetor x da iteração anterior
 
-print("x =", x)
+    # Nova estimação de x: x = Cx + g
+    x = np.dot(C, x) + g
+
+    # Cálculo vetor m
+    m = abs(x - x_anterior)
+
+    # Calculo de erro
+    mr = max(m).any() / max(x).any()
+
+    k+=1 # Incrementando contador
+
 print_x(x)
 
-# Testar solução
-x = list(x)
-x.append(1)
-print(A.dot(x)) 
+'''
+x_real = np.array([19.0/1489.0, -44.0/1489.0, 991.0/2978.0, 214.0/1489.0, 405.0/2978.0])
+print(x_real - x)
 
-
-
-
-
-
-
-
-
-    
-
-
-
-
-    
+t = list(x)
+t.append(1)
+print(np.dot(A, t))   
+'''
--- Modulo 2/gauss_seidel.py
+++ Modulo 2/gauss_seidel.py
@@ -9,8 +9,8 @@ Módulo 2 - Trabalho 9
 
 # Retorna um novo vetor ao "isolar" a variável da posição i
 def isolar(i: int, v: object):
-    new_v = v * (-1/v[i])
-    new_v[i] = 0
+    new_v = np.array(v * (-1/v[i]), dtype= "float64")
+    new_v[i] = 0.0
     return new_v
 
 # Printa o vetor x
@@ -18,67 +18,70 @@ def print_x(v: object):
     for i in range(v.shape[0]):
         print(("x" + str(i + 1) + " ≈"), v[i])
 
-# Exemplo 2
-'''
-A = [
-    [3, -0.1, -0.2, -7.85],
-    [0.1, 7, -0.3, 19.3],
-    [0.3, -0.2, 10, -71.4]
-    ]
-'''
-x = [0, 0, 0]
-
 A = [
     [8, -3, 2, 1, 0, -1], 
     [0, -10, 1, 1, -2, -1/2],
     [1, 0, 9, -1, 1, -3],
     [-2, 1, 0, -11, 1, 3/2],
-    [-2, 0, 2, 0, 10, 2]
+    [-2, 0, 2, 0, 10, -2]
     ]
 x = [0, 0, 0, 0, 0]
 
+'''
+A = [
+    [2, 1, -20],
+    [1, 4, -45]
+    ]
+x = [0, 0]
+'''
+ 
 tolerancia = 0.02
 mr = 1
 
-A = np.array(A, dtype = np.double)
-x = np.array(x, dtype = np.double)
+A = np.array(A, dtype="float64")
+x = np.array(x, dtype="float64")
 
-# A.shape = (nº de linhas de A, nº de colunas de A)
-nl = A.shape[0] # nº de linhas de A
+# Número de linhas 
+nl = A.shape[0]
+# Número de colunas para a matriz C
+nc = nl
 
-# Matriz C: Matriz A sem as constantes
-nc = A.shape[1] - 1 # nº de colunas de C
-C = np.zeros((nl, nc), dtype = np.double)
-# Vetor g: constantes da matriz A
-g = np.zeros(nl, dtype = np.double)
+C = np.zeros((nl, nc))
+g = np.zeros(nl)
 
-# Obter C e g
+# Encontrando a matriz C e o vetor g
 for i in range(nl):
     aux = isolar(i, A[i])
     for j in range(nc):
         C[i][j] = aux[j]
     g[i] = aux[nc]
 
+# Contador
+k = 0
+
 # Iterando
-while mr > tolerancia:
-    x_anterior = np.array(x)
-    #x = C.dot(x_anterior) + g
+while mr >= tolerancia:
+    x_anterior = np.copy(x) # Vetor x da iteração anterior
+
+    # Nova estimação de x: x = Cx + g
     for i in range(nl):
-        x[i] = C[i].dot(x) + g[i]
+        x[i] = np.dot(C[i], x) + g[i]
 
     print(x)
-    m = max(abs(x - x_anterior))
-    mr = m / max(x)
 
+    # Cálculo vetor m
+    m = abs(x - x_anterior)
+    print(m)
+    # Calculo de erro
+    mr = max(m) / max(x)
+    
+    k+=1 # Incrementando contador
 
-print("x =", x)
+print("Número de iterações =", k)
 print_x(x)
 
-# Testar solução
-x = list(x)
-x.append(1)
-print(A.dot(x))
- 
+
+
 
 
 
