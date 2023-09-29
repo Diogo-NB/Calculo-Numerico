98c5d7f554236571ba2ad93af141fe1f908cf7fa
Diogo-NB <diogonunes.20022@gmail.com>
2023-09-29 16:31:44 -0300

    Add reg_exponencial

--- Modulo 2/Regressao/regressao.py
+++ Modulo 2/Regressao/regressao.py
@@ -4,12 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 
-def func_linear(x ,a1 = 0, a0 = 0):
-    return a1*x + a0
-
-def func_potencia(x, a = 0, b = 0):
-    return a * (x ** b)
-
 def reg_linear(x, y, showResults = True, ylabel = None, xlabel = None):
     # y = a1x + a0
     x = np.array(x, dtype="float64")
@@ -47,7 +41,7 @@ def reg_linear(x, y, showResults = True, ylabel = None, xlabel = None):
     for i in range(n):
         e = y[i] - a0 - a1*(x[i])
         Sr += e*e
-        St = (y[i] - avg_y) * (y[i] - avg_y)
+        St += (y[i] - avg_y) * (y[i] - avg_y)
 
     # Coeficiente de Determinação
     r2 = (St - Sr) / St
@@ -74,6 +68,58 @@ def reg_linear(x, y, showResults = True, ylabel = None, xlabel = None):
 
     return results
 
+def reg_exponencial(x, y, ylabel = None, xlabel = None):
+    # y = a * e^(bx) 
+    
+    # Linearizando
+    x_linear = np.array(x, dtype="float64")
+    y_linear = np.log(y)
+
+    # Tamanho de x e y
+    n = x_linear.shape[0]
+
+    results = reg_linear(x_linear, y_linear, showResults= True)
+
+    # Calculando os parametros para a função original
+    a = np.e ** (results['a0'])
+    b = results['a1']
+
+    # Soma dos quadrados dos resíduos estimados
+    Sr = 0
+    # Soma total dos quadrados em torno da média da variável dependente y
+    St = 0
+    # Média de y
+    avg_y = np.average(y)
+
+    for i in range(n):
+        # y = a * e^(bx)
+        e = y[i] - a * np.e ** (b*x[i])
+        Sr += e*e
+        St += (y[i] - avg_y) * (y[i] - avg_y)
+
+    # Coeficiente de Determinação
+    r2 = (St - Sr) / St
+    
+    # Novo resultado com os corretos parametros
+    results = {
+        'a': a,
+        'b': b,
+        'r²': r2
+        }
+    
+    print(results)
+
+    y_calculado = np.zeros(n)
+    
+    for i in range(n):
+        y_calculado[i] = a * (np.e ** (b*x[i]))
+
+    plt.plot(x, y, 'o', color='black')
+    plt.plot(x, y_calculado, color='red')
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.show()
+
 def reg_potencia(x, y, ylabel = None, xlabel = None):
     # y = a * x^b 
     
@@ -89,12 +135,27 @@ def reg_potencia(x, y, ylabel = None, xlabel = None):
     # Calculando os parametros para a função original
     a = 10 ** (results['a0'])
     b = results['a1']
+
+    # Soma dos quadrados dos resíduos estimados
+    Sr = 0
+    # Soma total dos quadrados em torno da média da variável dependente y
+    St = 0
+    # Média de y
+    avg_y = np.average(y)
+
+    for i in range(n):
+        e = y[i] - a * (x[i]) ** b
+        Sr += e*e
+        St += (y[i] - avg_y) * (y[i] - avg_y)
+
+    # Coeficiente de Determinação
+    r2 = (St - Sr) / St
     
     # Novo resultado com os corretos parametros
     results = {
         'a': a,
         'b': b,
-        'r²': results['r²']
+        'r²': r2
         }
     
     print(results)
@@ -116,4 +177,4 @@ y = [126, 121, 116, 118, 114, 118, 132, 141, 108]
 ex_x = [10, 20, 30,   40,  50,   60,  70,   80]
 ex_y = [25, 70, 380, 550, 610, 1220, 830, 1450]
 
-reg_potencia(ex_x, ex_y)
+reg_exponencial(ex_x, ex_y)
