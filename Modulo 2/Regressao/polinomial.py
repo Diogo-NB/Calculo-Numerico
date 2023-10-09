610300329ddd1c534d42c8382ef300b923dc2897
Diogo-NB <diogonunes.20022@gmail.com>
2023-10-09 16:24:49 -0300

    FInalized reg_polinomial

--- Modulo 2/Regressao/polinomial.py
+++ Modulo 2/Regressao/polinomial.py
@@ -10,6 +10,7 @@ Módulo 2 - Trabalho 12
 """
 def reg_polinomial(x, y):
     n = len(x)
+    # y = a0 + a1x +a2x^2
 
     sx = sy = sx2 = sx3 = sx4 = sxy = sx2y = 0
 
@@ -32,7 +33,40 @@ def reg_polinomial(x, y):
     
     C = [sy, sxy, sx2y]
 
-    return (A, C)
+    a = gauss_seidel(A, C, [0, 0, 0], 0.000000002)
+
+    # Soma dos quadrados dos resíduos estimados
+    Sr = 0
+    # Soma total dos quadrados em torno da média da variável dependente y
+    St = 0
+    # Média de y
+    avg_y = np.average(y)
+
+    for i in range(n):
+        e = y[i] - a[0] - a[1] * (x[i]) - a[2] * (x[i]*x[i])
+        Sr += e * e
+        St += (y[i] - avg_y) * (y[i] - avg_y)
+
+    y_calculado = np.zeros(n)
+    
+    for i in range(n):
+        y_calculado[i] = a[0] + a[1]*(x[i]) + a[2] * (x[i] * x[i])
+
+    plt.plot(x, y, 'o', color='black')
+    plt.plot(x, y_calculado, color='red')
+    plt.show()
+
+    # Coeficiente de Determinação
+    r2 = (St - Sr) / St
+
+    results = {
+        "a0": a[0],
+        "a1": a[1],
+        "a2": a[2], 
+        "r2": r2
+        }
+    
+    return results
 
 # Retorna um novo vetor ao "isolar" a variável da posição i
 def isolar(i: int, v: object):
@@ -45,56 +79,56 @@ def print_x(v: object):
     for i in range(v.shape[0]):
         print(("x" + str(i + 1) + " ≈"), v[i])
 
-A = [
-    [8, -3, 2, 1, 0, -1], 
-    [0, -10, 1, 1, -2, -1/2],
-    [1, 0, 9, -1, 1, -3],
-    [-2, 1, 0, -11, 1, 3/2],
-    [-2, 0, 2, 0, 10, -2]
-    ]
-x = [0, 0, 0, 0, 0]
+# Ax = c
+def gauss_seidel(A, c, x, tolerancia):
+    mr = 1
 
-tolerancia = 0.02
-mr = 1
+    A = np.array(A, dtype="float64")
+    c = np.array(c, dtype="float64")
+    x = np.array(x, dtype="float64")
 
-A = np.array(A, dtype="float64")
-x = np.array(x, dtype="float64")
+    # Número de linhas 
+    nl = A.shape[0]
+    # Número de colunas para a matriz C
+    nc = nl
 
-# Número de linhas 
-nl = A.shape[0]
-# Número de colunas para a matriz C
-nc = nl
+    C = np.zeros((nl, nc))
+    g = np.zeros(nl)
 
-C = np.zeros((nl, nc))
-g = np.zeros(nl)
-
-# Encontrando a matriz C e o vetor g
-for i in range(nl):
-    aux = isolar(i, A[i])
-    for j in range(nc):
-        C[i][j] = aux[j]
-    g[i] = aux[nc]
-
-# Contador
-k = 0
-
-# Iterando
-while mr >= tolerancia:
-    x_anterior = np.copy(x) # Vetor x da iteração anterior
-
-    # Nova estimação de x: x = Cx + g
+    # Encontrando a matriz C e o vetor g
     for i in range(nl):
-        x[i] = np.dot(C[i], x) + g[i]
+        aux = isolar(i, A[i])
+        for j in range(nc):
+            C[i][j] = aux[j]
+        g[i] = c[i] / A[i][i]
 
-    print(x)
+    # Contador
+    k = 0
 
-    # Cálculo vetor m
-    m = abs(x - x_anterior)
-    print(m)
-    # Calculo de erro
-    mr = max(m) / max(x)
-    
-    k+=1 # Incrementando contador
+    # Iterando
+    while mr >= tolerancia:
+        x_anterior = np.copy(x) # Vetor x da iteração anterior
 
-print("Número de iterações =", k)
-print_x(x)
+        # Nova estimação de x: x = Cx + g
+        for i in range(nl):
+            x[i] = np.dot(C[i], x) + g[i]
+
+        # Cálculo vetor m
+        m = abs(x - x_anterior)
+        print(m)
+        # Calculo de erro
+        mr = max(m) / max(x)
+        
+        k+=1 # Incrementando contador
+
+    print("Número de iterações =", k)
+    print_x(x)
+    return x
+
+x = [1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0]
+y = [ 87,  74,  62,  59,  65,  71,   82,   94]
+
+ex_x = [1.5,  3, 4.5,   6,  7.5,   9,  10.5,   12]
+ex_y = [ 87, 74,  62,  59,   65,   71,   82,   94]
+
+print(reg_polinomial(ex_x, ex_y))
