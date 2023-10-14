72ee3081410f70e1ae9c735c23ce5a81e1b34cf4
Diogo-NB <113485393+Diogo-NB@users.noreply.github.com>
2023-10-14 17:07:09 -0300

    Final gauss_jacobi

--- modulo_2/gauss_jacobi.py
+++ modulo_2/gauss_jacobi.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import numpy as np
 
 """
@@ -18,69 +17,62 @@ def print_x(v: object):
     for i in range(v.shape[0]):
         print(("x" + str(i + 1) + " ≈"), v[i])
 
+# Retorna uma solução x para o sistema Ax = c usando o método de gauss_seidel
+def gauss_jacobi(A, c, x, tolerancia):
+    # Ax = c
+    # Parametro x é a estimativa inicial de solução para x 
+    mr = 1
+
+    A = np.array(A, dtype="float64")
+    c = np.array(c, dtype="float64")
+    x = np.array(x, dtype="float64")
+
+    # Número de linhas 
+    nl = A.shape[0]
+    # Número de colunas para a matriz C
+    nc = nl
+
+    C = np.zeros((nl, nc))
+    g = np.zeros(nl)
+
+    # Encontrando a matriz C e o vetor g
+    for i in range(nl):
+        aux = isolar(i, A[i])
+        for j in range(nc):
+            C[i][j] = aux[j]
+        g[i] = c[i] / A[i][i]
+
+    # Contador
+    k = 0
+
+    # Iterando
+    while mr > tolerancia:
+        x_anterior = np.copy(x) # Vetor x da iteração anterior
+
+        # Nova estimação de x: x = Cx + g
+        x = np.dot(C, x) + g
+
+        # Cálculo vetor m
+        m = abs(x - x_anterior)
+
+        # Calculo de erro
+        mr = max(m).any() / max(x).any()
+
+        k+=1 # Incrementando contador
+
+    print("Número de iterações =", k)
+    print_x(x)
+
+    return x
+
 A = [
-    [8, -3, 2, 1, 0, -1], 
-    [0, -10, 1, 1, -2, -1/2],
-    [1, 0, 9, -1, 1, -3],
-    [-2, 1, 0, -11, 1, 3/2],
-    [-2, 0, 2, 0, 10, -2]
+    [ 8,  -3,  2,   1,  0], 
+    [ 0, -10,  1,   1, -2],
+    [ 1,   0,  9,  -1,  1],
+    [-2,   1,  0, -11,  1],
+    [-2,   0,  2,   0, 10]
     ]
-x = [0, 0, 0, 0, 0]
+c = [ 1, 1/2,  3, -3/2, 2]
+x = [ 0,   0,  0,    0, 0]
 
-'''
-A = [
-    [2, 1, -20],
-    [1, 4, -45]
-    ]
-x = [0, 0]
-'''
- 
-tolerancia = 0.02
-mr = 1
-
-A = np.array(A, dtype="float64")
-x = np.array(x, dtype="float64")
-
-# Número de linhas 
-nl = A.shape[0]
-# Número de colunas para a matriz C
-nc = nl
-
-C = np.zeros((nl, nc))
-g = np.zeros(nl)
-
-# Encontrando a matriz C e o vetor g
-for i in range(nl):
-    aux = isolar(i, A[i])
-    for j in range(nc):
-        C[i][j] = aux[j]
-    g[i] = aux[nc]
-
-# Contador
-k = 0
-
-# Iterando
-while mr > tolerancia:
-    x_anterior = np.copy(x) # Vetor x da iteração anterior
-
-    # Nova estimação de x: x = Cx + g
-    x = np.dot(C, x) + g
-
-    # Cálculo vetor m
-    m = abs(x - x_anterior)
-
-    # Calculo de erro
-    mr = max(m).any() / max(x).any()
-
-    k+=1 # Incrementando contador
-
-print_x(x)
-
-'''
-x_real = np.array([19.0/1489.0, -44.0/1489.0, 991.0/2978.0, 214.0/1489.0, 405.0/2978.0])
-print(x_real - x)
-
-t = list(x)
-t.append(1)
-print(np.dot(A, t))   
-'''
+gauss_jacobi(A, c, x, 0.02)
