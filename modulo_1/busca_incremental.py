7bc43d4ae1a581829688dff70cdcce0bb5acfa21
Diogo-NB <diogonunes.20022@gmail.com>
2023-11-24 14:36:07 -0300

    Fix buscaIncremental and buscaSecanteModificada

--- modulo_1/busca_incremental.py
+++ modulo_1/busca_incremental.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import numpy as np
 """
 @author: Diogo Nunes Batista
@@ -7,38 +6,34 @@ Módulo 1 - Trabalho 1
 Estimação de raízes pelo método de busca incremental.
 """
 
-# Declaração da função
-def func(x): return 2*np.log(x-1) + 3
-
-# Criando o vetor do intervalo e o dividindo em 100 subintervalos
-vetor = np.linspace(1.002, 2, 101)
-
-i = 0
-a = vetor[i]
-b = vetor[i+1]
-
-# Enquanto não achar o subintervalo que possui a raíz ao nível aceito de erro
-while func(a)*func(b) >= 0:
-    # Próximo subintervalo
-    i+=1     
-    
-    # Se percorreu todos subintervalos e não foi encontrado
-    if (i >= vetor.size - 1): 
-        print("Não foi encontrada raíz")
-        break
-    
+def buscaIncremental(func, vetor, fx = 0): # fx é o número a ser buscado
+    n = len(vetor)
+    i = 0
     a = vetor[i]
     b = vetor[i+1]
-else:
-    print("Subintervalo onde a raíz se encontra: [", a,':',b, ']')
 
-    
-    
+    # Enquanto não achar o subintervalo que possui a raíz ao nível aceito de erro, usando o deslocamento (fx)
+    while (func(a) - fx)*(func(b) - fx) >= 0:
+        # Próximo subintervalo
+        i+=1     
+        
+        # Se percorreu todos subintervalos e não foi encontrado
+        if (i >= n - 1): 
+            return
+        
+        a = vetor[i]
+        b = vetor[i+1]
 
+    return (a, b)
 
+def trabalho_1():
+    # Declaração da função
+    def func(x): return 2*np.log(x-1) + 3
 
+    # Criando o vetor do intervalo e o dividindo em 100 subintervalos
+    vetor = np.linspace(1.002, 2, 101)
+    (a, b) = buscaIncremental(func, vetor)
+    print(f"Intervalo encontrado: {[a,b]}")
 
-
-
-
-
+if __name__ == "__main__":
+    trabalho_1()
--- modulo_1/secante_modificado.py
+++ modulo_1/secante_modificado.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 """
 @author: Diogo Nunes Batista
 
@@ -9,15 +7,15 @@ Estimação de raízes pelo método da secante modificado.
 
 def f(x): return (x**5)/3 - x**4 + x + 1
 
-ptb = 1e-6 # Pertubação
-erroMin = 1e-4 # Condição de parada, mínimo de erro aceito 
-xr1 = 2 # Estimativa inicial
-xr2 = xr1 - (ptb*xr1*f(xr1))/(f(xr1 + ptb*xr1) - f(xr1))  # Próxima estimativa
+def buscaSecanteModificado(f, xr1):
+    ptb = 1e-6 # Pertubação
+    erroMin = 1e-4 # Condição de parada, mínimo de erro aceito 
 
-# Enquanto não achar uma estimativa com nível aceitável de erro
-while abs((xr2 - xr1)/xr2)*100 >= erroMin:
-    xr1 = xr2
     xr2 = xr1 - (ptb*xr1*f(xr1))/(f(xr1 + ptb*xr1) - f(xr1))  # Próxima estimativa
-    
-print("Raiz estimada = ",xr2)
 
+    # Enquanto não achar uma estimativa com nível aceitável de erro
+    while abs((xr2 - xr1)/xr2)*100 >= erroMin:
+        xr1 = xr2
+        xr2 = xr1 - (ptb*xr1*f(xr1))/(f(xr1 + ptb*xr1) - f(xr1))  # Próxima estimativa
+    
+    return xr2
