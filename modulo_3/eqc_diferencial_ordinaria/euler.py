cf680670eb096e46199ad5bb6f91b04c124d09f9
Diogo-NB <diogonunes.20022@gmail.com>
2023-11-25 20:41:04 -0300

    Add euler.py

--- modulo_3/eqc_diferencial_ordinaria/euler.py
+++ modulo_3/eqc_diferencial_ordinaria/euler.py
@@ -6,6 +6,38 @@ Diogo Nunes Batista
 Trabalho 20
 """
 
+def metodo_euler(f, a, b, f0, h):
+    # Cria um vetor que vai de a até b com espaço
+    # de 'h' entre cada elemento
+    t = np.arange(a, b + h, h)
+    n = len(t)
+    y = np.zeros(n)
+    y[0] = f0 # Atribuindo a condição inicial à y[0]
+
+    for i in range(0, n-1):
+        y[i+1] = y[i] + f(t[i], y[i])*h
+    
+    return y
+
+"""
+Exemplo do slide
 f = lambda t, y: 4 * np.e ** (0.8 * t) - 0.5 * y
-y0 = 2 # Condição inicial, y(0) = t0
-passo = 1
+f0 = 2 # Condição inicial, f(0) = t0
+
+# Intervalo
+a = 0
+b = 4
+# passo
+h = 1
+"""
+f = lambda x, y: -2*y + 4 * np.e ** (-x)
+f0 = 2 # Condição inicial, f(0) = t0
+
+# Intervalo
+a = 0
+b = 1
+# passo
+h = 0.2
+
+y = metodo_euler(f, a, b, f0, h)
+print(f"{y=}")
