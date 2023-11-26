90d1b0974e9001217f449b6366ce4c038bc3ef4d
Diogo-NB <113485393+Diogo-NB@users.noreply.github.com>
2023-11-26 01:06:13 -0300

    Add runge_kutta.py

---
+++ modulo_3/eqc_diferencial_ordinaria/runge_kutta.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+"""
+Diogo Nunes Batista
+
+Trabalho 24
+"""
+
+def metodoHungeKutta(f, a, b, f0, h):
+    # Cria um vetor que vai de a até b com espaço
+    # de 'h' entre cada elemento
+    t = np.arange(a, b + h, h)
+    n = len(t)
+    y = np.zeros(n)
+    y[0] = f0 # Atribuindo a condição inicial à y[0]
+    
+    for i in range(0, n-1):
+        k1 = f(t[i], y[i])
+        k2 = f(t[i] + h/2, y[i] + k1*h/2)
+        k3 = f(t[i] + h/2, y[i] + k2*h/2)
+        k4 = f(t[i] + h, y[i] + k3*h)
+        y[i+1] = y[i] + (k1 + 2*k2 + 2*k3 + k4) * h / 6
+
+    return y
+
+"""
+#Exemplo do slide
+f = lambda t, y: 4 * np.e ** (0.8 * t) - 0.5 * y
+f0 = 2 # Condição inicial, f(0) = t0
+
+# Intervalo
+a = 0
+b = 1
+# passo
+h = 1
+"""
+f = lambda x, y: -2*y + 4 * np.e ** (-x)
+f0 = 2 # Condição inicial, f(0) = t0
+
+# Intervalo
+a = 0
+b = 1
+# passo
+h = 0.2
+
+y = metodoHungeKutta(f, a, b, f0, h)
+print(f"{y=}")
