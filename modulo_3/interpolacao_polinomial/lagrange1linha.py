cda3c87d4d530e0c56b124c72a4da830da1b8c87
Diogo-NB <113485393+Diogo-NB@users.noreply.github.com>
2023-12-06 22:12:17 -0300

    Update lagrange1linha.py

--- modulo_3/interpolacao_polinomial/lagrange1linha.py
+++ modulo_3/interpolacao_polinomial/lagrange1linha.py
@@ -2,18 +2,20 @@ import numpy as np
 from time import perf_counter
 
 """
-Implementação metodo de lagrange com apenas uma linha de codigo
-SIM, vc leu corretamente
+@author: Diogo Nunes Batista
+Polinômio interpolador de Lagrange usando apenas uma linha de código
 """
 
 # Retorna a estimação de f(t)
 def metodoLagrange(x, y, t): return np.sum([(np.prod([np.prod([(t - xj) / (x[i] - xj) for xj in x[:i]]), np.prod([(t - xj) / (x[i] - xj) for xj in x[i+1:]])]) * (y[i])) for i in range(len(x))])
 
-x = np.linspace(0, 10, 1000)
-y = np.sin(x)
-t = 5
+# Trabalho 15
+x = [25, 26, 27]
+y = [124, 154, 165]
+t = 25.8
+
 t1_start = perf_counter()
-print(f"f({t}) = {metodoLagrange(x, y, t)} e sin({t}) = {np.sin(t)}")
+print(f"f({t}) = {metodoLagrange(x, y, t)}")
 t1_stop = perf_counter()
  
 print("Time:", t1_stop-t1_start)
